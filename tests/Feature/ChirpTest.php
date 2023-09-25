0ce4a88ed211611e7a0dbd613c08fa708ee75fac
sifrious <mmebit@icloud.com>
2023-09-25 09:59:01 -0400

    passing tests on /chirps endpoint

--- tests/Feature/ChirpTest.php
+++ tests/Feature/ChirpTest.php
@@ -3,11 +3,8 @@
 use App\Models\Chirp;
 use App\Models\User;
 
-it('gives back an 302 response for the home page for a registered user', function () {
-
-    $response = $this->get('/');
-
-    $response->assertStatus(302);
+it('gives back an 302 response for the home page for a guest user', function () {
+    $this->get('/chirps')->assertRedirect();
 });
 
 it('gives back successful response for the home page for a registered user', function () {
